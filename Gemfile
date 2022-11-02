2253269dd946c5db9fae15ddafa0d3e9c43f8f16
Gavin Carew <gjcarew@gmail.com>
2022-11-01 18:18:34 -0700

    Work on testing oauth

--- Gemfile
+++ Gemfile
@@ -63,7 +63,7 @@ end
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'simplecov'
-  # gem 'selenium-webdriver'
+  gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   # gem 'chromedriver-helper'
   gem 'shoulda-matchers'
--- spec/features/landing/index_spec.rb
+++ spec/features/landing/index_spec.rb
@@ -4,11 +4,21 @@ RSpec.describe 'Landing page' do
   describe 'When I visit the root path' do 
     describe 'And I am not logged in' do 
       it 'They should see a link to log in with Google button' do 
-        # allow_any_instance_of(<controller>).to receive(response).and_return(accepted)
+        # stub_request(:get, "http://127.0.0.1:58296/__identify__").
+        #  with(
+        #    headers: {
+       	#   'Accept'=>'*/*',
+       	#   'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+       	#   'User-Agent'=>'Ruby'
+        #    }).
+        #  to_return(status: 200, body: "", headers: {})
+        Capybara.current_driver = :selenium
         visit root_path
+        save_and_open_page
         expect(page).to have_button('Log in with Google')
-        click_button 'Log in with Google'
-        expect(current_path).to eq(user_dashboard_path)
+        # # click_button 'Log in with Google'
+        # save_and_open_page
+        # expect(current_path).to eq(user_dashboard_path)
       end
     end
   end
