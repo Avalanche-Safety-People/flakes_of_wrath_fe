7906166b30cad47a0c3e96586b957381c0c2a656
Jake Kim <imjakekim@gmail.com>
2022-11-06 14:09:37 -0800

    skip new_users_spec tests

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'New trip' do 
   describe 'When I visit a new user page' do 
-    it 'I see my upcoming trips' do 
+    xit 'I see my upcoming trips' do 
       allow_any_instance_of(TripsController).to receive(:logged_in_user).and_return(true)
       allow_any_instance_of(TripsController).to receive(:current_user).and_return(bob)
       allow_any_instance_of(TripsController).to receive(:user_trips).and_return(bobs_trips)
 
     end
-    it 'When I fill out the form with valid data and click submit,
+    xit 'When I fill out the form with valid data and click submit,
         I am redirected to the user home page where I see my new trip' do 
       bob = UserFacade.get_user(1)
       bobs_trips = UserFacade.user_trips(1)
