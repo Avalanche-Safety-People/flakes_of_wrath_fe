a9dc9405397aacb3f9683d7722c51b9867d4f9da
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-11-07 15:25:51 -0700

    change the VCR tapes for heroku app

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -82,7 +82,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
   config.ignore_request { ENV['DISABLE_VCR'] }
-  # config.ignore_localhost = true
+  config.ignore_localhost = true
   config.default_cassette_options = { re_record_interval: 365.days }
   config.configure_rspec_metadata!
   #config.filter_sensitive_data('<API_KEY>', { ENV['API_KEY'] })
