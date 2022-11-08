c6d0fccc594218e83c67fb164d815edde5192057
Noah van Ekdom <noahvanekdom@gmail.com>
2022-11-08 10:27:54 -0700

    Add tests

--- spec/features/areas/index_spec.rb
+++ spec/features/areas/index_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Area Index Page' do
   describe "As a User, when I visit the area index page" do
 
-    it "Displays a list of all avalanche zones for the state", :vcr do
+    it "Displays a list of all avalanche zones for the state, including current avalanche danger", :vcr do
       areas = AreaFacade.all_zones
 
       visit areas_path
       expect(page).to have_content(areas.first.name)
       expect(page).to have_content(areas.last.name)
+      expect(page).to have_content(areas.first.current_av_risk)
     end
 
     it "has links that direct you to a show page for the zone", :vcr do
