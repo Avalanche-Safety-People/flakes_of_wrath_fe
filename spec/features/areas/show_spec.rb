d07ed6f086000f1f4ec1e485f59de4d8c7930882
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-11-09 14:24:24 -0700

    fixed failing tests from the changes

--- spec/features/areas/show_spec.rb
+++ spec/features/areas/show_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Area Show Page' do
       expect(page).to have_content "Olympics"
       expect(page).to have_content area.description
       expect(page).to have_content area.current_av_risk_string
-      expect(page).to have_content area.start_date
+      expect(page).to have_content area.end_date
       expect(page).to have_content area.av_forecast_string[1]
       expect(page).to have_content area.av_forecast_string[2]
       expect(page).to have_link "Check the NWAC forecast"
