7e03969c0f853e71cf082d31c9e6b9a34b65f76d
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-11-09 14:03:42 -0700

    styling added to forecast page

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -3,9 +3,11 @@ class AreasController < ApplicationController
 
   def index
     @areas = AreaFacade.all_zones
+    @area = AreaFacade.find_zone(current_user.favorite_zone)
   end
 
   def show
+    @areas = AreaFacade.all_zones
     @area = AreaFacade.find_zone(params[:id])
   end
 end
